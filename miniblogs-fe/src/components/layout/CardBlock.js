f4ec986d2e22ecd66b9282004f0c9264b4a2bc01
xuanphi99 <doanxuanphi2@gmail.com>
2021-10-07 08:40:16 +0700

    update demo

--- miniblogs-fe/src/components/layout/CardBlock.js
+++ miniblogs-fe/src/components/layout/CardBlock.js
@@ -6,7 +6,7 @@ export default class CardBlock extends Component {
             <div className="card about__info"  >
             <div className="card-body" style={{marginRight : '1.5rem'}}>
               <h4 className="card-title">Special title treatment</h4>
-              <p className="card-text">With supporting Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque qui illo quas, soluta consectetur ut quae aperiam excepturi fugiat ab voluptate tempore est architecto? Blanditiis nostrum iure tenetur sed iusto.lorem Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae, accusamus dolor earum facilis quos doloremque dolorum illum sunt! Modi eligendi natus numquam nisi officiis ab minima nostrum voluptas quae similique?  text below as a natural lead-in to additional content.</p>
+              <p className="card-text">With supporting Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque qui illo quas, soluta consectetur ut quae aperiam excepturi fugiat ab voluptate tempore est architecto? Blanbnbnbeligendi natus numquam nisi officiis ab minima nostrum voluptas quae similique?  text below as a natural lead-in to additional content.</p>
             </div>
           </div>
           
